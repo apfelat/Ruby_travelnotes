fa5977bdcf086472944d2333949b4bf10839471b
EC2 Default User <ec2-user@ip-172-31-15-40.ap-northeast-1.compute.internal>
2021-04-18 02:25:48 +0000

    0418

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :articles, -> { order("done ASC, position ASC").includes(:post) }, dependent: :destroy
-
  validates :title, presence: true
 end
